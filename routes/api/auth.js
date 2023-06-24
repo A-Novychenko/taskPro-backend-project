2a1d40831cfddf0d2efb0a83344829b4532f4053
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2023-06-24 20:45:58 +0300

    authenticate/logout

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
 const { validateBody } = require("../../middlewares");
+
+const { authenticate } = require("../../middlewares");
+
 const { schemas } = require("../../models/user");
 const {
   register,
@@ -13,6 +17,8 @@ router.post("/register", validateBody(schemas.registerSchema), register);
 
 router.post("/login", validateBody(schemas.loginSchema), login);
 
-// router.get("/current", authenticate, getCurrent);
+router.get("/current", authenticate, getCurrent);
+
+router.post("/logout", authenticate,  logout);
 
 module.exports = router;
