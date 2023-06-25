d477f8176116d3eb4cbb9884a90c314b3262295e
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-06-25 22:57:13 +0300

    fix add deadline - date/Joi schemas

--- models/task.js
+++ models/task.js
@@ -1,74 +1,87 @@
-const {Schema, model} = require("mongoose")
-const { handleMongooseError } = require("../helpers");
+const {Schema, model} = require("mongoose");
+const {handleMongooseError} = require("../helpers");
 const Joi = require("joi");
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  priority: {
-    type: String,
-    enum: ["priority1", "priority2", "priority3"],
-    default: "priority1"
-  },
-  deadline: {
-    type: Date,
-    default: Date.now,
-    require: true,
-  },
-  owner: {
-        type: Schema.Types.ObjectId,  
-        ref: 'column',                   
-        required: 'true',
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-}, { versionKey: false, timestamps: true });
+    description: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      enum: ["priority1", "priority2", "priority3"],
+      default: "priority1",
+    },
+    deadline: {
+      type: Date,
+      default: Date.now,
+      require: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "column",
+      required: "true",
+    },
+  },
+  {versionKey: false, timestamps: true}
+);
 
 const taskJoiSchema = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-})
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  deadline: Joi.date(),
+});
 
-const boardSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  owner: {
-        type: Schema.Types.ObjectId,  
-        ref: 'users',                   
-        required: 'true',
+const boardSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-}, { versionKey: false, timestamps: true });
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+      required: "true",
+    },
+  },
+  {versionKey: false, timestamps: true}
+);
 
 const boardJoiSchema = Joi.object({
-    title: Joi.string().required(),
-})
+  title: Joi.string().required(),
+});
 
-const columnSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  owner: {
-        type: Schema.Types.ObjectId,  
-        ref: 'board',                   
-        required: 'true',
+const columnSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-}, { versionKey: false, timestamps: true });
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "board",
+      required: "true",
+    },
+  },
+  {versionKey: false, timestamps: true}
+);
 
-const columnJoiSchema = Joi.object({
+const columnJoiSchema = Joi.object(
+  {
     title: Joi.string().required(),
-}, { versionKey: false, timestamps: true })
+  },
+  {versionKey: false, timestamps: true}
+);
 
 const schemas = {
   taskJoiSchema,
   boardJoiSchema,
-  columnJoiSchema
+  columnJoiSchema,
 };
 
 taskSchema.post("save", handleMongooseError);
