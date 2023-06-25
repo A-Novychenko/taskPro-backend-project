d30d13397bb0f924e5d5d91baa7441f394fb5efa
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-06-25 17:12:38 +0300

    fix getCurrent

--- controller/auth/getCurrent.js
+++ controller/auth/getCurrent.js
@@ -1,22 +1,13 @@
-const jwt = require("jsonwebtoken");
-
-const { SECRET_KEY } = process.env;
-
 const getCurrent = async (req, res) => {
-  const { user } = req.body;
-  const payload = {
-    id: user._id,
-  };
-
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  const {token, name, email} = req.user;
 
   res.json({
     status: "OK",
     code: 200,
     token,
     user: {
-      email: user.email,
-      name: user.name,
+      email,
+      name,
     },
   });
 };
