bc3999c0a820a65ffc3adf096aeb3b6a570d3be8
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-07-03 04:00:07 +0300

    fix

--- controller/tasks/deleteColumn.js
+++ controller/tasks/deleteColumn.js
@@ -3,10 +3,13 @@ const {Column} = require("../../models/task");
 
 const deleteColumn = async (req, res) => {
   const {columnId} = req.params;
+
   const column = await Column.findByIdAndRemove(columnId);
+
   if (!column) {
     throw HttpError(404, "Not found");
   }
+
   res.json({status: "success", code: 200, message: "Column deleted", column});
 };
 
